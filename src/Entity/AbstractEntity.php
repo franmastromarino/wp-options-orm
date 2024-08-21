7fa9fe5dbc0a47cca5d2b9831ce71a32e7037a85
Kenneth Neimann <kan_a_n@hotmail.com>
2024-08-21 12:44:03 -0300

    fix: fix abstractEntity class constructor

--- src/Entity/AbstractEntity.php
+++ src/Entity/AbstractEntity.php
@@ -12,16 +12,11 @@ abstract class AbstractEntity
 
     public function __construct(array $entityProperties = [])
     {
-
         // Get all non static properties of the entity
-        $entityReflection = new \ReflectionClass($this);
-        $entityProperties = $entityReflection->getProperties(\ReflectionProperty::IS_PUBLIC);
-        error_log('entityProperties: ' . json_encode($entityProperties, JSON_PRETTY_PRINT));
-
-        $entity_vars = get_object_vars($this);
+        $defaultProperties = get_object_vars($this);
 
         // Sanitize each property with sanitizeProperty method else throw an exception
-        $sanitizedProperties = getEntitySanitizedData($entityProperties, self::$sanitizeProperties, $this, get_class($this), $entity_vars);
+        $sanitizedProperties = getEntitySanitizedData($entityProperties, self::$sanitizeProperties, $this, get_class($this), $defaultProperties);
         
         // Validate each property with validateProperty method else throw an exception
         validateProperties($sanitizedProperties, self::$validateProperties, $this, get_class($this));
@@ -29,8 +24,5 @@ abstract class AbstractEntity
         foreach ($sanitizedProperties as $property => $value) {
             $this->$property = $value;
         }
-
-        // TODO: call to helpers function getEntitySanitizedData validateProperties when moved to helpers.
-        // This will help to sanitize/validate endpotins that have to handle data and not save it anywhere.
     }
 }
