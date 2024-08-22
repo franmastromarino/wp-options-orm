ae453b7a43fede17773288145f8aab03940f5aa8
Kenneth Neimann <kan_a_n@hotmail.com>
2024-08-22 09:19:35 -0300

    fix: static instead self and delete sanitizeProperties validateProperties

--- src/Entity/AbstractEntity.php
+++ src/Entity/AbstractEntity.php
@@ -7,19 +7,16 @@ use function QuadLayers\WP_Orm\Helpers\validateProperties;
 
 abstract class AbstractEntity extends SingleEntity
 {
-    public static $sanitizeProperties = [];
-    public static $validateProperties = [];
-
     public function __construct(array $entityProperties = [])
     {
         // Get all non static properties of the entity
         $defaultProperties = get_object_vars($this);
 
         // Sanitize each property with sanitizeProperty method else throw an exception
-        $sanitizedProperties = getEntitySanitizedData($entityProperties, self::$sanitizeProperties, $this, get_class($this), $defaultProperties);
+        $sanitizedProperties = getEntitySanitizedData($entityProperties, static::$sanitizeProperties, $this, get_class($this), $defaultProperties);
         
         // Validate each property with validateProperty method else throw an exception
-        validateProperties($sanitizedProperties, self::$validateProperties, $this, get_class($this));
+        validateProperties($sanitizedProperties, static::$validateProperties, $this, get_class($this));
 
         foreach ($sanitizedProperties as $property => $value) {
             $this->$property = $value;
