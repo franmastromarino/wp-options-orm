fd58759256a699f2af39ad3fce9729d4b198b0e0
Kenneth Neimann <kan_a_n@hotmail.com>
2024-08-29 10:00:24 -0300

    feat: add testGettersAndSetters test

--- tests/Single/SingleTest.php
+++ tests/Single/SingleTest.php
@@ -9,23 +9,27 @@ class SingleTest extends TestCase
 {
     public function testGettersAndSetters()
     {
+        $testValues = [
+            'key1' => 'new_value_1',
+            'key2' => 'new_value_2',
+            'key3' => '10',
+            'key4' => 10,
+            'key5' => 'New Test Alternative',
+        ];
 
         $entity = new SingleEntityTest();
 
-        // TODO: Implement setters
-        // $entity->setKey1('test');
-        // $entity->setKey2('test');
-        // $entity->setKey3('test');
-        // $entity->setKey4('test');
-        // $entity->setKey5('test');
+        $entity->set( 'key1', $testValues['key1']);
+        $entity->set( 'key2', $testValues['key2']);
+        $entity->set( 'key3', $testValues['key3']);
+        $entity->set( 'key4', $testValues['key4']);
+        $entity->set( 'key5', $testValues['key5']);
 
-        $testValue = $entity->getDefaults();
-
-        $this->assertEquals($testValue['key1'], $entity->getKey1());
-        $this->assertEquals($testValue['key2'], $entity->getKey2());
-        $this->assertEquals($testValue['key3'], $entity->getKey3());
-        $this->assertEquals($testValue['key4'], $entity->getKey4());
-        $this->assertEquals($entity->sanitizeKey5(), $entity->getKey5());
+        $this->assertEquals($testValues['key1'], $entity->getKey1());
+        $this->assertEquals($testValues['key2'], $entity->getKey2());
+        $this->assertEquals($testValues['key3'], $entity->getKey3());
+        $this->assertEquals($testValues['key4'], $entity->getKey4());
+        $this->assertEquals($testValues['key5'], $entity->getKey5());
     }
 
     // Test sanitize function
